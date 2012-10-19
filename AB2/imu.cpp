1a0fbb434032f3ed7150fe21a66e5a66fb7a02dd
Kevin Seidel <kevinseidel@ubuntu.(none)>
2012-10-19 17:41:16 +0200

    First implementation

--- AB2/imu.cpp
+++ AB2/imu.cpp
@@ -2,20 +2,41 @@
 #include <sensor_msgs/Imu.h>
 #include <tf/transform_datatypes.h>
 
+int count = 0;
+double rollsum, rollsumquad, pitchsum, pitchsumquad, yawsum, yawsumquad;
+
 void imuCallback(const sensor_msgs::Imu::ConstPtr& msg)
 {
 	tf::Quaternion q;
-	double roll,pitch,yaw;
-	tf::quaternionMsgToTF(msg->orientation,q);
-	tf::Matrix3x3(q).getRPY(roll,pitch,yaw);
-	ROS_INFO("Got IMU−Data:(%lf,%lf,%lf)",roll,pitch,yaw);
+	double roll, pitch, yaw;
+	tf::quaternionMsgToTF(msg->orientation, q);
+	tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
+	rollsum += roll;
+	pitchsum += pitch;
+	yawsum += yaw;
+
+	rollsumquad += roll * roll;
+	pitchsumquad += pitch * pitch;
+	yawsumquad += yaw * yaw;
+
+	++count;
+
+	if(count > 1)
+	{
+		roll = sqrt((1/(count-1))*(rollsumquad-((1/count)*(rollsum*rollsum))));
+		pitch = sqrt((1/(count-1))*(pitchsumquad-((1/count)*(pitchsum*pitchsum))));
+		yaw = sqrt((1/(count-1))*(yawsumquad-((1/count)*(yawsum*yawsum))));
+	}
+	ROS_INFO("Got IMU-Data: (%lf, %lf, %lf)", roll, pitch, yaw);
 }
-int main(int argc, char** argv)
+
+int main(int argc, char **argv)
 {
-	ros::init(argc,argv,"imu");
+	ros::init(argc, argv, "imu");
 	ros::NodeHandle n;
-	ros::Subscriber sub = n.subscribe("/imucorrected",1,imuCallback);
+	ros::Subscriber sub = n.subscribe("/imu_corrected", 1, imuCallback);
+	
 	ros::spin();
-	return 0 ;
+	return 0;
 }
 
